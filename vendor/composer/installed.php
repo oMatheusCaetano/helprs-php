a319a6aef7e98bc5c1e2ad2d456b3d5d1835960e
Matheus Caetano <caetanomatheus876@gmail.com>
2024-11-24 10:37:58 -0300

    1.3.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'matheuscaetano/helprs',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5322a8317ada799f08977e0dd29c47e650632375',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'matheuscaetano/helprs' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5322a8317ada799f08977e0dd29c47e650632375',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
